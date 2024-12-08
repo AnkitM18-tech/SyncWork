821fe1ab219b3f6e69ab1f6654c0ed5ab511e7ea
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-12-08 14:18:23 +0530

    initial setup

---
+++ sync-work/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <p>Home</p>
+      <Button>Click Here</Button>
+    </div>
+  );
+};
+
+export default Home;
