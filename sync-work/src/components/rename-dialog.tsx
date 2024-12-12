97ffd999c7fac773945b03f6753b71284d9e5d85
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-12-13 02:20:54 +0530

    Document Rename Dialog

---
+++ sync-work/src/components/rename-dialog.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+import { Id } from "../../convex/_generated/dataModel";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+interface RenameDialogProps {
+  documentId: Id<"documents">;
+  initialTitle: string;
+  children: React.ReactNode;
+}
+
+export const RenameDialog = ({
+  documentId,
+  initialTitle,
+  children,
+}: RenameDialogProps) => {
+  const update = useMutation(api.documents.updateById);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [title, setTitle] = useState(initialTitle);
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsUpdating(true);
+    update({
+      id: documentId,
+      title: title.trim() || "Untitled",
+    }).finally(() => {
+      setIsUpdating(false);
+      setOpenDialog(false);
+    });
+  };
+
+  return (
+    <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent onClick={(e) => e.stopPropagation()}>
+        <form onSubmit={onSubmit}>
+          <DialogHeader>
+            <DialogTitle>Rename Document</DialogTitle>
+            <DialogDescription>
+              Enter a new Name for this document
+            </DialogDescription>
+          </DialogHeader>
+          <div className="my-4">
+            <Input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Enter Document Name"
+              onClick={(e) => e.stopPropagation()}
+            />
+          </div>
+          <DialogFooter>
+            <Button
+              type="button"
+              variant="ghost"
+              disabled={isUpdating}
+              onClick={(e) => {
+                e.stopPropagation();
+                setOpenDialog(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              disabled={isUpdating}
+              onClick={(e) => e.stopPropagation()}
+              className="bg-blue-500"
+            >
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
